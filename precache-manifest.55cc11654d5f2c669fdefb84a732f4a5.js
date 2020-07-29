af4ba354bc5ae733856ee44a79c75edc4ee9b808
Hibiki Kuze <desu2dan@gmail.com>
2020-07-29 16:47:12 -0500

    Updates

---
+++ precache-manifest.55cc11654d5f2c669fdefb84a732f4a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532a384fe3685bea5caefc956fdb2839",
+    "url": "/eliminated-int-cyoa/index.html"
+  },
+  {
+    "revision": "4ed0697f8b5a2951667e",
+    "url": "/eliminated-int-cyoa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f48df5e6d6329cdb4982",
+    "url": "/eliminated-int-cyoa/static/js/2.38982421.chunk.js"
+  },
+  {
+    "revision": "c37b1bc9635b80e4a2e78366909bab16",
+    "url": "/eliminated-int-cyoa/static/js/2.38982421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed0697f8b5a2951667e",
+    "url": "/eliminated-int-cyoa/static/js/main.90cbb9c6.chunk.js"
+  },
+  {
+    "revision": "408aa0c292c469f219b7",
+    "url": "/eliminated-int-cyoa/static/js/runtime-main.1abac10f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/eliminated-int-cyoa/precache-manifest.55cc11654d5f2c669fdefb84a732f4a5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eliminated-int-cyoa/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
